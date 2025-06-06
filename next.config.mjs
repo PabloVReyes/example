f12af56ff7458dffc6b58f6abf3c23abfa8b87a0
Pablo Vazquez Reyes <PabloVazquezReyes@outlook.com>
2025-06-06 00:07:37 -0600

    Modificando para subir

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+    output: 'export',
+    basePath: '/example', // cambia esto
+    trailingSlash: true,
+};
 
 export default config;
